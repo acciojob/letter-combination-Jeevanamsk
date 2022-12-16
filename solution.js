86063ce76cc94b80c77c6e32527265ffdd8feb04
Jeevanamsk <113972063+Jeevanamsk@users.noreply.github.com>
2022-12-16 10:36:55 +0530

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,5 +1,25 @@
-function letterCombinations(input_digit) {
-  //Complete the function
-}
 
+
+
+
+function letterCombinations(digits) {
+  if (!digits || !digits.length) return [];
+
+  const mapping = ['0', '1', 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz'];
+  const result = [];
+
+  function dfs(curr, index) {
+    if (index === digits.length) {
+      result.push(curr);
+      return;
+    }
+    const letters = mapping[digits[index]];
+    for (let i = 0; i < letters.length; i++) {
+      dfs(curr + letters[i], index + 1);
+    }
+  }
+
+  dfs('', 0);
+  return result;
+}
 module.exports = letterCombinations;
